94616a3f00014287a2453a06000f4cfa2d48512e
Valentin Breuß <vbreuss@gmail.com>
2026-04-04 21:02:32 +0200

    feat: add initial analyzer that detects Moq

--- Tests/Mockolate.Migration.Example.Tests/Examples.cs
+++ Tests/Mockolate.Migration.Example.Tests/Examples.cs
@@ -7,7 +7,9 @@ public class Examples
 	[Fact]
 	public async Task MoqCreation()
 	{
+#pragma warning disable MockolateM001
 		Mock<IChocolateDispenser> sut = new();
+#pragma warning restore MockolateM001
 
 		sut.Setup(m => m.Dispense(It.IsAny<string>(), It.Is<int>(x => x > 0)))
 			.Returns(true);
